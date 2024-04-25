2d4eb0bf02293e606a17b8573d095516aa80f58e
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-04-26 01:04:41 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
          <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
          <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+         <link rel="icon" href="/fevicon.ico" />
       </head>
       <body classNameName={inter.classNameName}>
         <Header/>
