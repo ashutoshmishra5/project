7b76127bb21d1cda1a7f45ff2fb87b41a4958543
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-04-26 01:43:03 +0530

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 const Cards = ({src,title,href}) => {
     return(
-        <div classNameName="rounded-xl overflow-hidden bg-gray-200 hover:shadow-lg transform hover:-translate-y-1 duration-1000">
+        <div className="rounded-xl overflow-hidden bg-gray-200 hover:shadow-lg transform hover:-translate-y-1 duration-1000">
             <a href={href}>
-            <Image classNameName="h-40" src={src} alt="" width={400} height={400} />
-            <div classNameName="flex items-center justify-center bg-gray-200 font-semibold text-gray-500 text-md"><h1 classNameName="my-4">{title}</h1>
+            <div className="flex items-center justify-center"><Image className="h-40 w-auto" src={src} alt="" width={400} height={400} /></div>
+            <div className="flex items-center justify-center bg-gray-200 font-semibold text-gray-500 text-md"><h1 className="my-4 mx-4">{title}</h1>
             </div>
             </a>
     </div>
     );
 };
+
  export default Cards;
