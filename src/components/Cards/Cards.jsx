2875504201082ef7cc27f66a50cec0a7ff0fb409
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-04-26 01:48:21 +0530

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ const Cards = ({src,title,href}) => {
     return(
         <div className="rounded-xl overflow-hidden bg-gray-200 hover:shadow-lg transform hover:-translate-y-1 duration-1000">
             <a href={href}>
-            <div className="flex items-center justify-center"><Image className="h-40 w-auto rounded-xl" src={src} alt="" width={400} height={400} /></div>
+            <div className="flex items-center justify-center"><Image className="rounded-xl h-40 w-auto" src={src} alt="" width={400} height={400} /></div>
             <div className="flex items-center justify-center bg-gray-200 font-semibold text-gray-500 text-md"><h1 className="my-4 mx-4">{title}</h1>
             </div>
             </a>
